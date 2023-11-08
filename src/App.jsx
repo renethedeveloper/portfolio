5f65187b35f912569e0a205d008323d3ba57f69e
Rene Rodriguez <afuereno@gmail.com>
2023-11-07 21:59:42 -0800

    m

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-     <h1>My Portfolio</h1>
+     <h1>Rene's Portfolio</h1>
+     <h2>Projects:</h2>
+     <h2><a href="https://renethedeveloper.github.io/MOD.1.Project/">Dealer's Choice</a></h2>
+     <h3>Don't know what cocktail to make? Dealer's Choice offers  creative ideas for the home bartender. Mixology is just a button click away.</h3>
+     
+     <h3> <a href="https://www.linkedin.com/in/renethedeveloper/">Visit my LinkedIn Profile</a> </h3>
+     <h3> <a href="https://again-admin.onrender.com">Visit my MOD 3 Project</a> </h3>
+
     </>
   )
 }
